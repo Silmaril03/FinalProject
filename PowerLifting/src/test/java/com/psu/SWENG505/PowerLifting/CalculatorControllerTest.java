16c94afe00974cf94ffd492162d00d97e92cfce4
Jason <Jchinaka@gmail.com>
2017-02-03 19:50:09 -0800

    Repo Creation

---
+++ PowerLifting/src/test/java/com/psu/SWENG505/PowerLifting/CalculatorControllerTest.java
@@ -0,0 +1,25 @@
+package com.psu.SWENG505.PowerLifting;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class CalculatorControllerTest extends TestCase{
+
+	public CalculatorControllerTest(String testName){
+		super(testName);
+	}
+	
+	public void testCalculator(){
+		//instantiate Controller Class
+		assertTrue(false);
+	}
+	
+	public void testCalcWilksScore(){
+		assertTrue(false);
+	}
+	
+	public void testBodyMass(){
+		assertTrue(false);
+	}
+}
