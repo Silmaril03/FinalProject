6a73f9351ec3bb8110ebdacd8549e75c869306e7
Jason <Jchinaka@gmail.com>
2017-02-04 20:42:50 -0800

    Tests Addition

--- PowerLifting/src/test/java/com/psu/SWENG505/PowerLifting/CalculatorControllerTest.java
+++ PowerLifting/src/test/java/com/psu/SWENG505/PowerLifting/CalculatorControllerTest.java
@@ -1,25 +1,29 @@
 package com.psu.SWENG505.PowerLifting;
 
+import com.psu.SWENG505.PowerLifting.ejbs.CalculatorController;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class CalculatorControllerTest extends TestCase{
-
+	CalculatorController calc;
+	
 	public CalculatorControllerTest(String testName){
 		super(testName);
+		CalculatorController calc = new CalculatorController();
 	}
 	
 	public void testCalculator(){
 		//instantiate Controller Class
-		assertTrue(false);
+		assertNotNull(calc.Calculate());
 	}
 	
 	public void testCalcWilksScore(){
-		assertTrue(false);
+		assertNotNull(calc.CalcWilksScore(0, 0));
 	}
 	
 	public void testBodyMass(){
-		assertTrue(false);
+		assertNotNull(calc.CalcBodyMass());
 	}
 }
