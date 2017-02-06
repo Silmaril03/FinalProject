1d8cd5684b3be77f8afcbef0453592b05cb841a8
Jan Balangue <jantonio.balangue@gmail.com>
2017-02-05 23:27:16 -0800

    Add files via upload

---
+++ PowerLifting/src/main/java/com/psu/SWENG505/PowerLifting/ejbs/Exercise.java
@@ -0,0 +1,19 @@
+package com.psu.SWENG505.PowerLifting.ejbs;
+
+public class Exercise {
+	String exerciseName;
+	Boolean isPartOfTotal;
+	
+	public Exercise() {
+		
+	}
+	
+	public String getExerciseName() {
+		return getExerciseName();
+	}
+	
+	public void setCustomExercise() {
+		// set custom exercise
+	}
+
+}
---
+++ PowerLifting/src/main/java/com/psu/SWENG505/PowerLifting/ejbs/TrainingLogController.java
@@ -0,0 +1,18 @@
+package com.psu.SWENG505.PowerLifting.ejbs;
+
+import java.util.Calendar;
+
+public class TrainingLogController {
+	
+	public Workout getWorkout(Workout workout) {
+		return workout;
+	}
+	
+	public Boolean setWorkout(Calendar calendar) {
+		return false;
+	}
+	
+	public Boolean setGoal() {
+		return false;
+	}
+}
